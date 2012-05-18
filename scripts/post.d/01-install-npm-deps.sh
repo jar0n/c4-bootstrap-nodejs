d57242fb85b371bb3ec580ad90051c3555d9dfef
Richard Harvey <richard@squarecows.com>
2012-05-18 10:51:26 +0100

    Updating scripts for node user creation

--- scripts/post.d/01-install-npm-deps.sh
+++ scripts/post.d/01-install-npm-deps.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Firstly set up the node user
+
+useradd --home-dir /home/node -s /bin/bash
+chown -Rf node.node /home/node
+
+# now install npm modules
+
 if [[ -f scripts/post.d/npm_deps ]]
 then
     install=(`cat scripts/post.d/npm_deps`)
     for i in "${install[@]}"
 	do
-	    sudo npm install ${i} -g
+	    su node -c "npm install express"
 	done 
 else
     echo "WARNING NOTHING CONFIGURED INSTALL!!!"
--- scripts/pre.d/01-install-nodejs.sh
+++ scripts/pre.d/01-install-nodejs.sh
@@ -5,6 +5,8 @@ apt-get install -y python-software-properties
 
 add-apt-repository -y ppa:richarvey/nodejs
 
+apt-get update
+
 # Now install nodeJS and NPM package manager
 
 apt-get install -y nodejs npm
